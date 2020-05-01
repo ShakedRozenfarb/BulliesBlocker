b9db4564139a447f40f8b1484bbb7d71484cb3e9
ShakedRozenfarb <54974786+ShakedRozenfarb@users.noreply.github.com>
2020-05-02 00:38:52 +0300

    Add files via upload
    Using Twitter API to get user last X tweets

---
+++ Server/src/twitterApiConfig.py
@@ -0,0 +1,4 @@
+consumer_key = 'DnWYbGhVpO4LkA8il2JNFe8qK'
+consumer_secret = 'oPej0uzY1aJVAp1MDQkQx8Blz5AJH5kque63muHFHyhLJAfLZB'
+access_token = '1247861692865839111-Fc03yxnq0abpNRCT41v9hsExJ3rSQd'
+access_token_secret = 'XxIIJguVHoRN0tAQJ4aU2XmJWBKRvD3319CWlebjpoaSu'
