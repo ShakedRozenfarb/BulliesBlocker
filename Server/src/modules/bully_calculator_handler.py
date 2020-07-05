253a013879c24f71996b4a05a6212e17a94fdb1e
Or Gaizer <orgaizer@microsoft.com>
2020-07-05 16:14:20 +0300

    Support undefined

--- Server/src/modules/bully_calculator_handler.py
+++ Server/src/modules/bully_calculator_handler.py
@@ -13,6 +13,7 @@ caching_mongo_client = MongoHandler().get_caching_mongo_client()
 def calculate_bully_score(user):
     timeline = get_user_time_line(IS_SCREEN_NAME, user, TWEETS_COUNT)
     bully_counter = 0
+    undefined_counter = 0
     non_bully_counter = 0
     cached_tweets = []
     tweets_to_calculate = []
@@ -31,21 +32,30 @@ def calculate_bully_score(user):
     tweets_messages_to_calculate = [tweet_to_calculate["tweet_message"] for tweet_to_calculate in tweets_to_calculate]
 
     if tweets_to_calculate:
-        tweets_tesult = predictLabels(tweets_messages_to_calculate)
+        tweets_result = predictLabels(tweets_messages_to_calculate)
         for index in range(len(tweets_to_calculate)):
-            tweets_to_calculate[index]["is_bully"] = False if tweets_tesult[index] == 0 else True
+            if tweets_result[index] == 0:
+                tweets_to_calculate[index]["is_bully"] = False
+            elif tweets_result[index] == 1:
+                tweets_to_calculate[index]["is_bully"] = True
+            else:
+                tweets_to_calculate[index]["is_bully"] = "undefined"
         caching_mongo_client.insert_many(tweets_to_calculate)
 
     all_tweets = cached_tweets + tweets_to_calculate
     for tweet in all_tweets:
         if tweet.get("_id"):
             del tweet["_id"]
-        if tweet.get("is_bully"):
+        tweet_result = tweet.get("is_bully")
+        if tweet_result == "undefined":
+            undefined_counter = undefined_counter + 1
+        elif tweet_result:
             bully_counter = bully_counter + 1
         else:
             non_bully_counter = non_bully_counter + 1
 
     return {
         "score": bully_counter / (bully_counter + non_bully_counter),
-        "tweets": all_tweets
+        "tweets": all_tweets,
+        "undefined": undefined_counter
     }
