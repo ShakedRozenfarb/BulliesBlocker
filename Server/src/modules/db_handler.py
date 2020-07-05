7813c036e203dced96464915cc4f3b18e01703bf
Or Gaizer <orgaizer@microsoft.com>
2020-07-05 16:14:44 +0300

    Support undefined

--- Server/src/modules/db_handler.py
+++ Server/src/modules/db_handler.py
@@ -4,7 +4,7 @@ from pymongo import MongoClient
 class MongoHandler(object):
     def __init__(self):
         self.host = "127.0.0.1"
-        self.port = 27018
+        self.port = 27017
         self.mongo_client = MongoClient(host=self.host, port=self.port)["bullyblocker"]
 
     def get_mongo_client(self):
